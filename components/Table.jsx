807b498bf2a2ea7ae1d8cfd7a728ddbe671e9bf5
Gautam Arora <gautamarora6248@gmail.com>
2022-06-23 20:49:12 +0530

    add: Add filtered table for SelectInstance route

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,81 +1,127 @@
-import React from "react";
-import { useTable } from "react-table";
+import React from 'react'
+import styled from 'styled-components'
+import { useTable, useRowSelect } from 'react-table'
 
-const Table = ({ columns, data }) => {
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
-        columns,
-        data
-    });
-    return (
-        <table {...getTableProps()}>
-            <thead>
-                {headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map((column) => (
-                            <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map((row, i) => {
-                    prepareRow(row);
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map((cell) => {
-                                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-                            })}
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
-    );
-};
 
-function TableComponent() {
-    const columns = React.useMemo(
-        () => [
-            {
-                Header: " ",
-                accessor: "rowName"
-            },
-            {
-                Header: "Max",
-                accessor: "max"
-            },
-            {
-                Header: "Avg",
-                accessor: "avg"
-            },
-            {
-                Header: "Last",
-                accessor: "last"
-            }
-        ],
-        []
-    );
 
-    const data = [
+
+const IndeterminateCheckbox = React.forwardRef(
+    ({ indeterminate, ...rest }, ref) => {
+        const defaultRef = React.useRef()
+        const resolvedRef = ref || defaultRef
+
+        React.useEffect(() => {
+            resolvedRef.current.indeterminate = indeterminate
+        }, [resolvedRef, indeterminate])
+
+        return (
+            <>
+
+                <input type="checkbox" ref={resolvedRef} {...rest} />
+            </>
+        )
+    }
+)
+
+function Table({ columns, data, showCheckbox }) {
+    // Use the state and functions returned from useTable to build your UI
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+        selectedFlatRows,
+        state: { selectedRowIds },
+    } = useTable(
         {
-            rowName: "CPU %",
-            max: "4%",
-            avg: "9.3%",
-            last: "2.3%"
+            columns,
+            data,
         },
-        {
-            rowName: "CPU %",
-            max: "4%",
-            avg: "9.3%",
-            last: "2.3%"
+        useRowSelect,
+        hooks => {
+            hooks.visibleColumns.push(columns => [
+                // Let's make a column for selection
+                {
+                    id: 'selection',
+                    Cell: ({ row }) => {
+                        if (
+                            rows.filter((row) => row.isSelected).length < 1 ||
+                            row.isSelected
+                        ) {
+                            return (
+                                <div>
+                                    <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div>
+                                    <IndeterminateCheckbox
+                                        checked={false}
+                                        readOnly
+                                        style={row.getToggleRowSelectedProps().style}
+                                    />
+                                </div>
+                            );
+                        }
+
+                    },
+                },
+                ...columns,
+            ])
         }
-    ];
+    )
+
+    // Render the UI for your table
+    return (
+        <>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.slice(0, 10).map((row, i) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            {/* <pre>
+                <code>
+                    {JSON.stringify(
+                        {
+                            selectedRowIds: selectedRowIds,
+                            'selectedFlatRows[].original': selectedFlatRows.map(
+                                d => d.original
+                            ),
+                        },
+                        null,
+                        2
+                    )}
+                </code>
+            </pre> */}
+        </>
+    )
+}
+
+function TableComponent({ columns, data, showCheckbox }) {
 
     return (
-        <div style={{ padding: "1rem" }}>
-            <Table columns={columns} data={data} />
-        </div>
-    );
+        <Table columns={columns} data={data} showCheckbox={showCheckbox} />
+    )
 }
 
 export default TableComponent;
--- pages/selectInstance.jsx
+++ pages/selectInstance.jsx
@@ -1,24 +1,78 @@
 import { useState, useMemo } from "react";
+import TableComponent from "../components/Table";
+import data from "../constants/data"
+
+
+
+function filterData(filterValue) {
+    const result = data.filter(item => item.class === filterValue);
+    const final_result = [];
+    result.map(item => {
+        const obj = {};
+        // TODO: Destructuring
+        obj.monthly = item.price.monthly;
+        obj.hourly = item.price.hourly;
+        obj.memory = item.memory;
+        obj.vcpus = item.vcpus;
+        obj.disk = item.disk;
+        obj.transfer = item.transfer;
+        obj.network_out = item.network_out;
+        final_result.push(obj);
+    })
+    return final_result;
+}
+
+const columns = [
+    {
+        Header: "Montly",
+        getToggleAllRowsSelectedProps: false,
+        accessor: "monthly"
+    },
+    {
+        Header: "Hourly",
+        accessor: "hourly"
+    },
+    {
+        Header: "RAM",
+        accessor: "memory"
+    },
+    {
+        Header: "CPUs",
+        accessor: "vcpus"
+    },
+    {
+        Header: "Storage",
+        accessor: "disk"
+    },
+    {
+        Header: "Transfer",
+        accessor: "transfer"
+    },
+    {
+        Header: "Network Out",
+        accessor: "network_out"
+    },
+]
 const tabs = [
     {
         id: 1,
         name: "Standard",
-        comp: "" //Table to be rendered with filtered rows
+        comp: <TableComponent columns={columns} data={filterData("standard")} showCheckbox={true} />
     },
     {
         id: 2,
         name: "High memory",
-        comp: "" //Table to be rendered with filtered rows
+        comp: <TableComponent columns={columns} data={filterData("highmem")} showCheckbox={true} />
     },
     {
         id: 3,
         name: "Nanode",
-        comp: "" //Table to be rendered with filtered rows
+        comp: <TableComponent columns={columns} data={filterData("nanode")} showCheckbox={true} />
     },
     {
         id: 4,
         name: "Dedicated",
-        comp: "" //Table to be rendered with filtered rows
+        comp: <TableComponent columns={columns} data={filterData("dedicated")} showCheckbox={true} />
     }
 ];
 
